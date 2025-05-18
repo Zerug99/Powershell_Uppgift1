96d3084a2279c26c684e970c2565f0cf3b25482e
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 18:39:29 +0200

    fortsätting med mappar

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -4,4 +4,7 @@ Write-Host "Det hemliga namnet blir" $name # Skriver ut variablen som har angets
 
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\" # Inne i mappen kommer vårt verktyg jobba i. Dom flesta har ett C: 
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\"  # Det användaren har angett kommer att bli mappen. 
+    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\logs\" 
+    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\scripts\"
+    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\temp\"
 
