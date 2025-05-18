f626264ef89d71a75d8678d41a75652070f1a2cf
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 18:29:31 +0200

    Huvudmappen för övriga mapparna

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -2,4 +2,6 @@ $name = Read-Host "Ange det hemliga namnet"  # Det användaren skriver kommer at
 
 Write-Host "Det hemliga namnet blir" $name # Skriver ut variablen som har angets här 
 
-New-Item -ItemType Directory -Path "C:\Strukturverktyg\" 
+New-Item -ItemType Directory -Path "C:\Strukturverktyg\"
+
+New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\"
