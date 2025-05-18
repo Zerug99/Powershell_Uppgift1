4acbfeef59126a7dc4fa4d9b2c1e16266d523129
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 18:51:44 +0200

    fel

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -8,6 +8,6 @@ Write-Host "Det hemliga namnet blir" $name # Skriver ut variablen som har angets
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\scripts\"
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\temp\"
 
-    New-item -ItemType file -path "C:\Script\$name\logs\log-YYYY-MM-DD.txt" #Första logge
-    
+    New-item -ItemType file -Path "C:\Scripts\$name\logs\log-YYYY-MM-DD.txt" #Första logge
+    Add-Content -Path "C:\Script\$name\logs\log-YYYY-MM-DD.txt" -Value "Struktur skapad: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 
