653e948054b0b01a8e33af4f9ec1d61ab644eddb
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 19:45:30 +0200

    kommentaren och slippar koden

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -1,31 +1,31 @@
-Function Skapa_Struktur {
+Function Skapa_Struktur {  # namnet på funktionen 
     param (
         [string]$namn
     )
 
     $sökväg = "C:\Strukturverktyg\$namn"
 
-    try {
-        # Kolla om mappen redan finns
+    try { # våran try den kommer att försöka kolla om mappen finns redan.
+        
         if (Test-Path $sökväg) {
             throw "Mappen finns redan!"
         }
 
-        # Skapa mappar
-        New-Item -ItemType Directory -Path "C:\Strukturverktyg\" 
-        New-Item -ItemType Directory -Path "$sökväg\" 
+        
+        New-Item -ItemType Directory -Path "C:\Strukturverktyg\" # Inne i mappen kommer vårt verktyg jobba i. Dom flesta har ett C:
+        New-Item -ItemType Directory -Path "$sökväg\" # Det användaren har angett kommer att bli mappen.
         New-Item -ItemType Directory -Path "$sökväg\logs\" 
         New-Item -ItemType Directory -Path "$sökväg\scripts\" 
         New-Item -ItemType Directory -Path "$sökväg\temp\" 
 
-        # Skapa loggfil
-        New-Item -ItemType File -Path "$sökväg\logs\log1.txt" 
+    
+        New-Item -ItemType File -Path "$sökväg\logs\log1.txt" #logfilen och anger ett cmdlet som ska ska out
         Add-Content -Path "$sökväg\logs\log1.txt" -Value "Struktur skapad: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 
         Write-Host "Klart strukturen finns i $sökväg"
 
-    } catch {
-        # Felmeddelande om något går fel
+    } catch {    # har skapar vi vårt felmeddelande
+        
         Write-Host "Fel: $_"
         Write-Host "Mappen kunde inte skapas"
     }
