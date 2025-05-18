3dd4b56867028755e788aed0397db5eb194f19ab
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 18:41:57 +0200

    fortsätting med mappar

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -8,3 +8,6 @@ Write-Host "Det hemliga namnet blir" $name # Skriver ut variablen som har angets
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\scripts\"
     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\temp\"
 
+    New-item -ItemType file -path "C:\Script\$name\logs\log-YYYY-MM-DD.txt" 
+    
+
