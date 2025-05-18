b8ed24f7cf93922f51b50c3602f35c859106622a
Patryk Jersak <patryk-99@hotmail.com>
2025-05-18 19:11:57 +0200

    jobb med funktionen

--- Uppgift1.ps1
+++ Uppgift1.ps1
@@ -1,13 +1,22 @@
+Function  The-Creator {
+    param (
+        [string]$namn
+    )
+
+
+
+
+
 $name = Read-Host "Ange det hemliga namnet"  # Det användaren skriver kommer att sparas i variabeln $name
 
 Write-Host "Det hemliga namnet blir" $name # Skriver ut variablen som har angets här 
 
-    New-Item -ItemType Directory -Path "C:\Strukturverktyg\" # Inne i mappen kommer vårt verktyg jobba i. Dom flesta har ett C: 
-    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\"  # Det användaren har angett kommer att bli mappen. 
-    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\logs\" 
-    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\scripts\"
-    New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\temp\"
+     New-Item -ItemType Directory -Path "C:\Strukturverktyg\" # Inne i mappen kommer vårt verktyg jobba i. Dom flesta har ett C: 
+     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\"  # Det användaren har angett kommer att bli mappen. 
+     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\logs\" 
+        New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\scripts\"
+     New-Item -ItemType Directory -Path "C:\Strukturverktyg\$name\temp\"
 
-    New-item -ItemType file -Path "C:\Strukturverktyg\$name\logs\log1.txt" #Första logge
-    Add-Content -Path "C:\Strukturverktyg\$name\logs\log1.txt" -Value "Struktur skapad: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+        New-item -ItemType file -Path "C:\Strukturverktyg\$name\logs\log1.txt" #Första logge
+        Add-Content -Path "C:\Strukturverktyg\$name\logs\log1.txt" -Value "Struktur skapad: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 
